86e268ccdf3b2f96b1c42b847157142d5c3f4465
Jan Strauß <jan@over9000.eu>
2016-06-16 18:24:56 +0200

    add api dummy + use osm tiles

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
-use iron::Iron;
+use iron::{Iron, Request, Response, IronResult};
+use iron::status;
 use staticfile::Static;
 use mount::Mount;
 
@@ -8,8 +9,14 @@ pub fn start() {
     let mut mount = Mount::new();
 
     mount.mount("/", Static::new(Path::new("web/")));
+    mount.mount("/api", send_hello);
 
     println!("server running on http://localhost:8080/");
 
     Iron::new(mount).http("127.0.0.1:8080").unwrap();
+}
+
+fn send_hello(req: &mut Request) -> IronResult<Response> {
+    println!("Running send_hello handler, URL path: {:?}", req.url.path);
+    Ok(Response::with((status::Ok, "Hello!")))
 }
